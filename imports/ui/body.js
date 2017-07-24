75240028f2a10dbdecc7b86df5e1bfb9df20f1c8
Rony Vidaur <rony.vidaur@gmail.com>
2017-07-23 22:00:45 -0600

    add task feature

---
+++ imports/ui/body.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating'
+import { Tasks } from '../api/tasks.js'
+
+import './body.html'
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({})
+  }
+})
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault()
+
+    const target = event.target
+    const text = target.text.value
+
+    //insert a task to the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    })
+    // clear form
+    target.text.value = ''
+  }
+})
